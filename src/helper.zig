d9a945e70f59a07136995025705f435906bbf7c2
kryo1337 <kryo1337@gmail.com>
2025-02-22 09:20:09 +0100

    type shit

--- src/helper.zig
+++ src/helper.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const windows = std.os.windows;
 const kernel32 = windows.kernel32;
+const c = @import("std").c;
+
+const c_void = anyopaque;
 
 const INT = windows.INT;
 const BOOL = windows.BOOL;
@@ -12,7 +15,63 @@ const LPVOID = windows.LPVOID;
 const HMODULE = windows.HMODULE;
 const HINSTANCE = windows.HINSTANCE;
 
+extern "user32" fn mouse_event(dwFlags: DWORD, dx: DWORD, dy: DWORD, dwData: DWORD, dwExtraInfo: usize) callconv(WINAPI) void;
 extern "user32" fn GetAsyncKeyState(vKey: INT) callconv(WINAPI) SHORT;
+extern "user32" fn GetDC(hWnd: ?*c_void) callconv(windows.WINAPI) ?*c_void;
+extern "gdi32" fn CreateCompatibleDC(hdc: ?*c_void) callconv(windows.WINAPI) ?*c_void;
+extern "gdi32" fn CreateCompatibleBitmap(hdc: ?*c_void, cx: i32, cy: i32) callconv(windows.WINAPI) ?*c_void;
+extern "gdi32" fn SelectObject(hdc: ?*c_void, h: ?*c_void) callconv(windows.WINAPI) ?*c_void;
+extern "gdi32" fn BitBlt(destDC: ?*c_void, x: i32, y: i32, width: i32, height: i32, srcDC: ?*c_void, srcX: i32, srcY: i32, rop: windows.DWORD) callconv(windows.WINAPI) windows.BOOL;
+extern "gdi32" fn DeleteDC(hdc: ?*c_void) callconv(windows.WINAPI) windows.BOOL;
+extern "gdi32" fn ReleaseDC(hWnd: ?*c_void, hdc: ?*c_void) callconv(windows.WINAPI) i32;
+extern "gdi32" fn DeleteObject(ho: ?*c_void) callconv(windows.WINAPI) windows.BOOL;
+extern "gdi32" fn GetObject(h: ?*c_void, nCount: i32, lpObject: *anyopaque) callconv(windows.WINAPI) i32;
+extern "gdi32" fn GetDeviceCaps(hdc: ?*c_void, index: i32) callconv(windows.WINAPI) i32;
+extern "gdi32" fn GetDIBits(hdc: ?*c_void, hbm: ?*c_void, start: u32, cLines: u32, lpvBits: *u8, lpbi: *BITMAPINFO, usage: u32) callconv(windows.WINAPI) i32;
+
+const SRCCOPY: windows.DWORD = 0x00CC0020;
+const DESKTOPHORZRES: i32 = 118;
+const DESKTOPVERTRES: i32 = 117;
+const MOUSEEVENTF_LEFTDOWN: DWORD = 0x0002;
+const MOUSEEVENTF_LEFTUP: DWORD = 0x0004;
+const BI_RGB: u32 = 0;
+const DIB_RGB_COLORS: u32 = 0;
+
+pub const BITMAP = packed struct {
+    bmType: i32,
+    bmWidth: i32,
+    bmHeight: i32,
+    bmWidthBytes: i32,
+    bmPlanes: u16,
+    bmBitsPixel: u16,
+    bmBits: ?*c_void,
+};
+
+pub const BITMAPFILEHEADER = packed struct {
+    bfType: u16,
+    bfSize: u32,
+    bfReserved1: u16,
+    bfReserved2: u16,
+    bfOffBits: u32,
+};
+
+pub const BITMAPINFOHEADER = packed struct {
+    biSize: u32,
+    biWidth: i32,
+    biHeight: i32,
+    biPlanes: u16,
+    biBitCount: u16,
+    biCompression: u32,
+    biSizeImage: u32,
+    biXPelsPerMeter: i32,
+    biYPelsPerMeter: i32,
+    biClrUsed: u32,
+    biClrImportant: u32,
+};
+
+pub const BITMAPINFO = packed struct {
+    bmiHeader: BITMAPINFOHEADER,
+};
 
 var frequency: u64 = 0;
 var start: u64 = 0;
@@ -29,6 +88,156 @@ pub const Config = struct {
     tap_time: f64 = 100,
     hold_mode: i32 = 1,
 };
+
+pub fn getScreenshot(save_name: ?[]const u8, crop_width: i32, crop_height: i32) ?*u32 {
+    var allocator = std.heap.c_allocator;
+
+    const screen_dc = GetDC(null);
+    if (screen_dc == null) {
+        std.debug.print("ERROR: GetDC() failed!\n", .{});
+        return null;
+    }
+
+    const screen_width = GetDeviceCaps(screen_dc, DESKTOPHORZRES);
+    const screen_height = GetDeviceCaps(screen_dc, DESKTOPVERTRES);
+
+    const crop_x = (screen_width - crop_width) / 2;
+    const crop_y = (screen_height - crop_height) / 2;
+
+    const mem_dc = CreateCompatibleDC(screen_dc);
+    if (mem_dc == null) {
+        std.debug.print("ERROR: CreateCompatibleDC() failed!\n", .{});
+        _ = ReleaseDC(null, screen_dc);
+        return null;
+    }
+
+    const bitmap = CreateCompatibleBitmap(screen_dc, crop_width, crop_height);
+    if (bitmap == null) {
+        std.debug.print("ERROR: CreateCompatibleBitmap() failed!\n", .{});
+        _ = ReleaseDC(null, screen_dc);
+        _ = DeleteDC(mem_dc);
+        return null;
+    }
+
+    _ = SelectObject(mem_dc, bitmap);
+
+    if (BitBlt(mem_dc, 0, 0, crop_width, crop_height, screen_dc, crop_x, crop_y, SRCCOPY) == 0) {
+        std.debug.print("ERROR: BitBlt() failed!\n", .{});
+        _ = ReleaseDC(null, screen_dc);
+        _ = DeleteDC(mem_dc);
+        _ = DeleteObject(bitmap);
+        return null;
+    }
+
+    var bmp: BITMAP = undefined;
+    if (GetObject(bitmap, @sizeOf(BITMAP), &bmp) == 0) {
+        std.debug.print("ERROR: GetObject() failed!\n", .{});
+        _ = ReleaseDC(null, screen_dc);
+        _ = DeleteDC(mem_dc);
+        _ = DeleteObject(bitmap);
+        return null;
+    }
+
+    var bmp_file_header = BITMAPFILEHEADER{
+        .bfType = 0x4D42,
+        .bfSize = @as(u32, @sizeOf(BITMAPFILEHEADER) + @sizeOf(BITMAPINFOHEADER) + (bmp.bmWidthBytes * crop_height)),
+        .bfReserved1 = 0,
+        .bfReserved2 = 0,
+        .bfOffBits = @as(u32, @sizeOf(BITMAPFILEHEADER) + @sizeOf(BITMAPINFOHEADER)),
+    };
+
+    var bmp_info_header = BITMAPINFOHEADER{
+        .biSize = @as(u32, @sizeOf(BITMAPINFOHEADER)),
+        .biWidth = bmp.bmWidth,
+        .biHeight = bmp.bmHeight,
+        .biPlanes = 1,
+        .biBitCount = 32,
+        .biCompression = BI_RGB,
+        .biSizeImage = @as(u32, bmp.bmWidthBytes * bmp.bmHeight),
+        .biXPelsPerMeter = 0,
+        .biYPelsPerMeter = 0,
+        .biClrUsed = 0,
+        .biClrImportant = 0,
+    };
+
+    var file: ?std.fs.File = null;
+    if (save_name) |sname| {
+        const fs = std.fs;
+        var cwd = fs.cwd();
+        file = cwd.createFile(sname, .{ .truncate = true }) catch {
+            std.debug.print("ERROR: fopen() failed!\n", .{});
+            _ = ReleaseDC(null, screen_dc);
+            _ = DeleteDC(mem_dc);
+            _ = DeleteObject(bitmap);
+            return null;
+        };
+
+        _ = file.writeAll(std.mem.bytesFrom(&bmp_file_header, @sizeOf(BITMAPFILEHEADER))) catch {
+            std.debug.print("ERROR: writing header failed!\n", .{});
+            file.close();
+            _ = ReleaseDC(null, screen_dc);
+            _ = DeleteDC(mem_dc);
+            _ = DeleteObject(bitmap);
+            return null;
+        };
+        _ = file.writeAll(std.mem.bytesFrom(&bmp_info_header, @sizeOf(BITMAPINFOHEADER))) catch {
+            std.debug.print("ERROR: writing info header failed!\n", .{});
+            file.close();
+            _ = ReleaseDC(null, screen_dc);
+            _ = DeleteDC(mem_dc);
+            _ = DeleteObject(bitmap);
+            return null;
+        };
+    }
+
+    const pixelBufferSize = bmp.bmWidthBytes * crop_height;
+    const numPixels = @as(usize, pixelBufferSize) / 4;
+    var pixels = allocator.alloc(u32, numPixels) catch {
+        std.debug.print("ERROR: malloc() failed!\n", .{});
+        if (file) |f| {
+            f.close();
+        }
+        _ = ReleaseDC(null, screen_dc);
+        _ = DeleteDC(mem_dc);
+        _ = DeleteObject(bitmap);
+        return null;
+    };
+
+    var bi: BITMAPINFO = .{
+        .bmiHeader = bmp_info_header,
+    };
+    if (GetDIBits(mem_dc, bitmap, 0, crop_height, @as(*u8, pixels), &bi, DIB_RGB_COLORS) == 0) {
+        std.debug.print("ERROR: GetDIBits() failed!\n", .{});
+        if (file) |f| {
+            f.close();
+        }
+        allocator.free(pixels);
+        _ = ReleaseDC(null, screen_dc);
+        _ = DeleteDC(mem_dc);
+        _ = DeleteObject(bitmap);
+        return null;
+    }
+
+    if (file) |f| {
+        const data_bytes = std.mem.bytesFrom(pixels[0..numPixels]);
+        _ = f.writeAll(data_bytes) catch {
+            std.debug.print("ERROR: writing pixel data failed!\n", .{});
+            f.close();
+            allocator.free(pixels);
+            _ = ReleaseDC(null, screen_dc);
+            _ = DeleteDC(mem_dc);
+            _ = DeleteObject(bitmap);
+            return null;
+        };
+        f.close();
+    }
+
+    _ = ReleaseDC(null, screen_dc);
+    _ = DeleteDC(mem_dc);
+    _ = DeleteObject(bitmap);
+
+    return pixels.ptr;
+}
 
 fn getRed(rgb: u32) i32 {
     return @as(i32, (rgb >> 16) & 0xFF);
@@ -173,6 +382,10 @@ pub fn stopCounter() void {
     reaction_count += 1;
     total_reaction += elapsed_time;
     std.debug.print("\rReaction time: {:.2} ms ", .{getReactionAverage(total_reaction, reaction_count)});
+}
+
+pub fn leftClick() void {
+    mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 }
 
 pub fn isKeyPressed(hold_key: i32) bool {
--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,45 @@
 const std = @import("std");
 const helper = @import("helper.zig");
 
+const E = error{ScreenshotFailed};
+
 pub fn main() !void {
-    std.debug.print("xd", .{});
+    const stdout = std.io.getStdOut().writer();
+    const stdin = std.io.getStdIn();
+
+    var red: i32 = 0;
+    var green: i32 = 0;
+    var blue: i32 = 0;
+
+    const cfg = helper.Config;
+
+    helper.getValorantColors(cfg.target_color, &red, &green, &blue);
+
+    helper.initPerformanceCounters();
+    const pixel_count = cfg.scan_area_x * cfg.scan_area_y;
+
+    try stdout.print("triggerbot is running\n", .{});
+
+    while (true) {
+        helper.startCounter();
+
+        if (cfg.hold_mode == 0 or helper.isKeyPressed(helper.getKeyCode(cfg.hold_key))) {
+            const pPixels_opt = helper.getScreenshot(null, cfg.scan_area_x, cfg.scan_area_y);
+            if (pPixels_opt == null) {
+                try stdout.print("ERROR: getScreenshot() failed!\nPress enter to exit: ", .{});
+                _ = stdin.readUntilDelimiterOrEof(&[_]u8{'\n'});
+                return E.ScreenshotFailed;
+            }
+            const pPixels = pPixels_opt.?;
+            const pixels_slice = std.mem.sliceFromPtr(pPixels, pixel_count);
+            if (helper.isColorFound(pixels_slice, red, green, blue, cfg.color_sens)) {
+                helper.leftClick();
+                helper.stopCounter();
+                try std.time.sleep(std.time.millisecond * @as(u64, cfg.tap_time));
+            }
+            std.heap.c_allocator.free(pPixels);
+        } else {
+            try std.time.sleep(std.time.millisecond);
+        }
+    }
 }
